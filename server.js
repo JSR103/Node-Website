27fbbd849889c355d2dfdeefdd53b4fec7d28a4b
JSR103 <silva.joel.r@gmail.com>
2017-06-30 22:18:04 -0700

    added bootstrap and a temple of bootstrap

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const nodemailer = require('nodemailer');
 
 var app = express();
 
+var smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    auth: {
+        user: "no.reply.jsr@gmail.com",
+        pass: "AwesomeAnime+103"
+    }
+});
+
+app.use(express.static(__dirname + '/startbootstrap-freelancer'));
 hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(__dirname + '/views/partials/aboutView');
+hbs.registerPartials(__dirname + '/views/partials/constantsPViews');
 
 app.set('view engine', 'hbs');//middleware
 
@@ -24,9 +37,9 @@ app.use((req,res,next) =>{
 // app.use((req,res,next) =>{
 //   res.render('maintenance.hbs');
 // });
-
 app.use(express.static(__dirname + '/public/css'));
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules/bootstrap'));
 
 hbs.registerHelper('getCurrentYear',() =>{
   return new Date().getFullYear();
@@ -50,13 +63,23 @@ app.get('/about', (req,res) =>{
     });
 });
 
-app.get('/bad', (req,res) =>{
-
-  res.send({
-    errorMessage: 'Error: Unable to handle request.'
-  });
-});
-
+app.get('/send',function(req,res){
+    var mailOptions={
+        to : req.query.email,
+        subject : req.query.subject,
+        text : req.query.content
+    }
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+     if(error){
+            console.log(error);
+        res.end("error");
+     }else{
+            console.log("Message sent: " + res.message);
+        res.end("sent");
+         }
+       });
+     });
 app.listen(3000, () =>{
   console.log('Server us up on port 3000');
 });
